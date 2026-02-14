8c949230eb7d6fbd89172b8a3dd6bd8091e82e1e
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-02-14 13:59:37 +0545

    updateqno3

---
+++ Questions/qno3.py
@@ -0,0 +1,4 @@
+# Convert Celsius to Fahrenheit.
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")

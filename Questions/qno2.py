c18552aea6b045aa3fe8d354ab5ba9209f85a19c
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-02-14 13:57:58 +0545

    updateqno2

---
+++ Questions/qno2.py
@@ -0,0 +1,7 @@
+# Take user input of name and age and print a formatted message.
+
+name=input("enter your name: ")
+age=input("enter your age: ")
+print(f"Hello i am {name}")
+print(f"i am {age} years old")
+      

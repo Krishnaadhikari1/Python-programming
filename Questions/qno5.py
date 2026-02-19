a3e6c6bb0282ff24ad910195ec0b84c314813963
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-02-19 21:14:47 +0545

    qno5

---
+++ Questions/qno5.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number  "))
+b=int(input("enter the number  "))
+c=int(input("enter the number  "))
+if a>b and a>c:
+    print("the greatest number is ",a)
+elif b>a and b>c:
+    print("the greatest number is ",b)
+else:    
+    print("the greatest number is ",c) 

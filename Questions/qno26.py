a73417c9b72eeae7f8d3126d740e9d2ab70a179c
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-07-04 20:23:11 +0545

    qno26

---
+++ Questions/qno26.py
@@ -0,0 +1,9 @@
+# Count frequency of elements using dictionary.
+numbers = [1, 2, 2, 3, 1, 4, 2, 3]
+frequency = {}
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+print(frequency)
